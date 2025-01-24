b8fbafb1c72b47789f48ca1412ae64ead102343f
muammar88 <muammarkadafi88@gmail.com>
2025-01-24 16:33:02 +0700

    init project amra

---
+++ client/src/components/ExampleUseApi.vue
@@ -0,0 +1,15 @@
+<!--
+import api from '@/api/axiosInstance';
+
+const sendData = async () => {
+  try {
+    const response = await api.post('/users', {
+      name: 'John Doe',
+      email: 'john@example.com',
+    });
+
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}; -->
