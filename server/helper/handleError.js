383b91aeab6167d2913442eb6c2c6ae55028ce9f
muammar88 <muammarkadafi88@gmail.com>
2025-01-25 15:32:57 +0700

    fix

---
+++ server/helper/handleError.js
@@ -0,0 +1,75 @@
+const { validationResult } = require("express-validator");
+
+const MESSAGES = {
+  ADD_SUCCESS: "Add Success",
+  ADD_FAILURE: "Add Failure",
+  UPDATE_SUCCESS: "Update Success",
+  UPDATE_FAILURE: "Update Failure",
+  DELETE_SUCCESS: "Delete Success",
+  DELETE_FAILURE: "Delete Failure",
+  INTERNAL_ERROR: "Internal Server Error",
+  VALIDATION_ERROR: "Data yang dimasukkan tidak valid",
+  DATA_NOTFOUND: "Data tidak ditemukan",
+  DATA_FOUNDED: "Data ditemukan",
+};
+
+const helper = {};
+
+helper.error_msg = async (errors) => {
+  let num = 0;
+  let err_msg = "";
+
+  console.log("+++++++++++++error_msg");
+  console.log("error_msg");
+  console.log(errors);
+  console.log(typeof errors);
+  console.log("+++++++++++++error_msg");
+  
+  errors.array().forEach((error) => {
+    if (num != 0) err_msg += "<br>";
+    err_msg += error.msg;
+    num++;
+  });
+  return err_msg;
+};
+
+helper.handleValidationErrors = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err_msg = await helper.error_msg(errors);
+    res.status(400).json({ error: true, error_msg: err_msg });
+    return false;
+  }
+  return true;
+};
+
+helper.handleValidationErrorsFiles = async (req, res, files) => {
+  const errors = validationResult(req);
+  for (let x in files) {
+    if (req.files[files[x].path] === undefined) {
+      errors.push = {
+        value: "",
+        msg: `File ${files[x].path} Wajib Diupload.`,
+        param: "file",
+        location: "body",
+      };
+    }
+  }
+
+  if (!errors.isEmpty()) {
+    const err_msg = await helper.error_msg(errors);
+    res.status(400).json({ error: true, error_msg: err_msg });
+    return false;
+  }
+  return true;
+};
+
+helper.handleServerError = (res, message = MESSAGES.INTERNAL_ERROR) => {
+  res.status(500).json({ error: true, error_msg: message });
+};
+
+// helper.messageError = () => {
+//   return MESSAGES;
+// };
+
+module.exports = helper;
---
+++ server/router/example.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { body } = require("express-validator");
+const controllers = require("../modules/example/controller/index");
+const validation = require("../validation/example");
+
+//------MIDDLEWARE-----//
+const { verify_session, verify_token } = require("../../../middleware/verify_session_token");
+
+// ROUTER
+const router = express.Router();
+
+router.get("/Example", controllers.Administrator);
+
+router.post("/Example/server_side",
+  [verify_session],
+  body("perpage").notEmpty().withMessage("Nomor Page Tidak Boleh Kosong").isNumeric().withMessage("Halaman harus dalam angka").trim(), 
+  body("search").trim(), 
+  controllers.server_side
+);
+
+
+
+
+module.exports = router;
