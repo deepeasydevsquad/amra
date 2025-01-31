ec576c46a1170101d6dbf6c0b24d42d16692b2d7
muammar88 <muammarkadafi88@gmail.com>
2025-01-31 23:09:56 +0700

    tambah branch

--- server/router/example.js
+++ server/router/example.js
@@ -12,8 +12,8 @@ const router = express.Router();
 router.get("/Example", controllers.Administrator);
 
 router.post("/Example/server_side",
-  [verify_session],
-  body("perpage").notEmpty().withMessage("Nomor Page Tidak Boleh Kosong").isNumeric().withMessage("Halaman harus dalam angka").trim(), 
+  [verify_token],
+  body("perpage").notEmpty().withMessage("Nomor Page Tidak Boleh Kosong").isNumeric().withMessage("Halaman harus dalam angka").trim().custom(validation.check_id), 
   body("search").trim(), 
   controllers.server_side
 );
