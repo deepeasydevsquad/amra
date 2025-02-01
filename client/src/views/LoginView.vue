c7fccc7966fe3a00bbabe605d5780eb8ccf5bbf7
GenProject05 <mrifkiauliap2105@gmail.com>
2025-02-02 02:09:22 +0700

    SCRUM-14 Created LoginView.vue, added assets, components (particles, widgets), and configured TailwindCSS

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LoginCard from "@/components/Login/widgets/LoginCard.vue";
+</script>
+
+
+<template>
+  <div class="bg-gray-100 flex items-center justify-center h-screen">
+    <LoginCard />
+  </div>
+</template>
+
