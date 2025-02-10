36a31dbe165333615ccb1a14f131e484a88efacb
muammar88 <muammarkadafi88@gmail.com>
2025-02-10 21:56:46 +0700

    fix

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -7,11 +7,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('@/views/AboutView.vue'),
-    },
-    {
       path: '/user/login',
       name: 'login',
       component: LoginView,
