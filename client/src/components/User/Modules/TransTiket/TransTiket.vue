fae9d744f0250e6057831cfe694e7b2316f0b2b1
muammar88 <muammarkadafi88@gmail.com>
2025-06-27 15:27:15 +0700

    x

--- client/src/components/User/Modules/TransTiket/TransTiket.vue
+++ client/src/components/User/Modules/TransTiket/TransTiket.vue
@@ -3,9 +3,6 @@
      <!-- Tambah data dan Search -->
      <div class="flex justify-between mb-4" >
     <PrimaryButton @click="startTicketTransaction">
-        <!-- <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-        </svg> -->
         <i class="pi pi-plus"></i>
         Mulai Transaksi Tiket
     </PrimaryButton>
