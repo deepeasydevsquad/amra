d3b58b849a782e57ff2b0b123e76cdc891aaa606
Muhammad Rifki Aulia Pratama <mrifkiauliap2105@gmail.com>
2025-07-10 19:39:16 +0700

    SCRUM-8 (FIXING) Refactoring daftar_kota components serta menjaga konsistensi nama router

--- server/router/router_daftar_kota.js
+++ server/router/router_daftar_kota.js
@@ -2,15 +2,12 @@ const express = require("express");
 const { body, param } = require("express-validator");
 const controllers = require("../modules/daftar_kota/controllers/index");
 const { authenticateToken } = require("../middleware/authenticateToken");
-// const getCompanyId = require("../middleware/getCompanyId");
 const validation = require("../validation/daftar_kota");
 
 const router = express.Router();
 
-// router.get("/daftar_kota", authenticateToken, controllers.get_daftar_kota);
-
 router.post(
-  "/daftar_kota/list",
+  "/daftar-kota/list",
   authenticateToken,
   [
     body("pageNumber").trim(),
@@ -21,43 +18,28 @@ router.post(
 );
 
 router.post(
-  "/daftar_kota/",
+  "/daftar-kota/add",
   authenticateToken,
   [
-    body("kode").trim().notEmpty().withMessage("Kode Kota tidak boleh kosong.").custom(validation.check_add_kode_kota),
+    body("kode").trim().notEmpty().withMessage("Kode Kota tidak boleh kosong.").toUpperCase().custom(validation.check_add_kode_kota),
     body("name").trim().notEmpty().withMessage("Nama Kota tidak boleh kosong."),
   ],
   controllers.add
 );
 
 router.post(
-  "/daftar_kota/update",
+  "/daftar-kota/update",
   authenticateToken,
   [
     body("id").trim().notEmpty().withMessage("ID Kota tidak boleh kosong.").custom(validation.check_id_kota),
-    body("kode").trim().notEmpty().withMessage("Kode Kota tidak boleh kosong.").custom(validation.check_edit_kode_kota),
+    body("kode").trim().notEmpty().withMessage("Kode Kota tidak boleh kosong.").toUpperCase().custom(validation.check_edit_kode_kota),
     body("name").trim().notEmpty().withMessage("Nama Kota tidak boleh kosong."),
   ],
   controllers.update
 );
 
-// update
-
-// //
-// router.put(
-//   "/daftar_kota/:id",
-//   authenticateToken,
-//   getCompanyId,
-//   [
-//     param("id").isInt().withMessage("ID harus berupa angka."),
-//     body("kode").trim().notEmpty().withMessage("Kode tidak boleh kosong."),
-//     body("name").trim().notEmpty().withMessage("Nama tidak boleh kosong."),
-//   ],
-//   controllers.update_daftar_kota
-// );
-
 router.post(
-  "/daftar_kota/delete",
+  "/daftar-kota/delete",
   authenticateToken,
   [body("id").trim().notEmpty().withMessage("ID Kota tidak boleh kosong.").isInt().withMessage("ID Kota harus berupa angka.").custom(validation.check_id_kota)],
   controllers.delete
