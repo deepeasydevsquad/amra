68d2ac9e290eb31482f27940bca4c838912a4e1d
darma <darmagans349@gmail.com>
2025-07-12 10:35:09 +0700

    x

--- client/src/components/User/Modules/PembayaranFeeAgen/PembayaranFeeAgen.vue
+++ client/src/components/User/Modules/PembayaranFeeAgen/PembayaranFeeAgen.vue
@@ -43,6 +43,8 @@ import {
   add_pembayaran_fee,
 } from '@/service/pembayaran_fee_agen'
 import { on } from 'events'
+import IconPlus from '@/components/Icons/IconPlus.vue'
+import IconMoney from '@/components/Icons/IconMoney.vue'
 
 const validateForm = (): boolean => {
   let isValid = true
@@ -287,17 +289,7 @@ const cetak_invoice = (invoice: string) => {
     <div class="flex justify-between items-center mb-6">
       <!-- Tombol Tambah di kiri -->
       <PrimaryButton @click="showModal = true" class="flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M2 4a2 2 0 012-2h16a2 2 0 012 2v4H2V4zm0 6h20v10a2 2 0 01-2 2H4a2 2 0 01-2-2V10zm4 4a1 1 0 000 2h4a1 1 0 000-2H6z"
-          />
-        </svg>
-
+        <IconMoney />
         Bayar Fee Agen
       </PrimaryButton>
 
