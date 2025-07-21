2daebc8131cf267519561baf7814c0dab845cb8b
muammar88 <muammarkadafi88@gmail.com>
2025-07-21 18:49:05 +0700

    x

---
+++ server/migrations/20250720100814-create-ppob-pascabayar-produk.js
@@ -0,0 +1,47 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Ppob_pascabayar_produks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      ppob_pascabayar_kategori_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Ppob_pascabayar_kategoris",
+          key: "id",
+        },
+        onDelete: 'CASCADE',
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      fee: {
+        type: Sequelize.INTEGER
+      },
+      markup: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['active', 'inactive'],
+        defaultValue : "inactive"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Ppob_pascabayar_produks');
+  }
+};
