a43dfecc012d57ecbbfbbeec64aae5799a52c40a
muammar88 <muammarkadafi88@gmail.com>
2025-07-23 11:33:46 +0700

    x

--- server/migrations/20250723024503-create-ppob-transaction-prabayar.js
+++ server/migrations/20250723024503-create-ppob-transaction-prabayar.js
@@ -9,9 +9,6 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      // company_id: {
-      //   type: Sequelize.INTEGER
-      // },
       company_id: {
         type: Sequelize.INTEGER,
         references: {
