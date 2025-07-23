85a39280894451931d654f460ec4e7c37640d07f
muammar88 <muammarkadafi88@gmail.com>
2025-07-23 12:01:53 +0700

    x

--- server/models/request_deposit_company.js
+++ server/models/request_deposit_company.js
@@ -4,13 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Request_deposit_company extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       Request_deposit_company.belongsTo(models.Company, {
         foreignKey: "company_id",
       });
