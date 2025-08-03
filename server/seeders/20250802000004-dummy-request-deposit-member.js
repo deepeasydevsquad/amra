8442f019afed9aeb95620a5466c4ed79d459e665
Muhammad Rifki Aulia Pratama <mrifkiauliap2105@gmail.com>
2025-08-04 02:47:22 +0700

    SCRUM-55: (ADDED) Add dummy data for deposit requests in seeder

--- server/seeders/20250802000004-dummy-request-deposit-member.js
+++ server/seeders/20250802000004-dummy-request-deposit-member.js
@@ -23,9 +23,35 @@ module.exports = {
         code: 456,
         status: 'disetujui',
         status_note: 'Permintaan deposit telah disetujui.',
+        petugas: "PT. MUHANDIS QUR'ANI WISATA",
+        akun_bank_perusahaan_id: null,
+        sending_payment_status: 'sudah_dikirim',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        company_id: 1,
+        member_id: 2,
+        nominal: 750000,
+        code: 456,
+        status: 'diproses',
+        status_note: 'Menunggu konfirmasi pembayaran.',
         petugas: null,
         akun_bank_perusahaan_id: null,
         sending_payment_status: 'sudah_dikirim',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        company_id: 1,
+        member_id: 3,
+        nominal: 1200000,
+        code: 456,
+        status: 'diproses',
+        status_note: 'Menunggu konfirmasi pembayaran.',
+        petugas: null,
+        akun_bank_perusahaan_id: null,
+        sending_payment_status: 'belum_dikirim',
         createdAt: new Date(),
         updatedAt: new Date()
       }
