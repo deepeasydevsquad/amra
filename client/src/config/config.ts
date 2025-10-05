40f4314cc66cb2c0354124db686f0e870fe19348
muammar88 <muammarkadafi88@gmail.com>
2025-10-05 16:28:43 +0700

    x

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = window.location.protocol + '//' + window.location.hostname + `:3002`;
+export const API_URL = window.location.protocol + '//' + window.location.hostname + `:3001`;
